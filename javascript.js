c2f615281bb45c41921682a8e91860980713ce71
DZAKYALR <0dzakyalr@gmail.com>
2020-11-20 14:07:17 +0700

    backend complete

---
+++ javascript.js
@@ -0,0 +1,277 @@
+function tally()
+
+        {
+        condt = 0;
+        if (document.guicheck.Item1.checked) { condt = condt + 1;}
+        if (document.guicheck.Item2.checked) { condt = condt + 2;}
+        if (document.guicheck.Item3.checked) { condt = condt + 4;}
+        if (document.guicheck.Item4.checked) { condt = condt + 8;}
+		    if (document.guicheck.Item5.checked) { condt = condt + 16;}
+		    if (document.guicheck.Item6.checked) { condt = condt + 32;}
+        condt = (condt);
+		
+if ( (condt) == 0)
+{output = "Angka yang anda fikirkan adalah 0";}
+
+if ( (condt) == 1)
+{output = "Angka yang anda fikirkan adalah 1";}
+
+if ( (condt) == 2)
+{output = "Angka yang anda fikirkan adalah 2";}
+
+if ( (condt) == 3)
+{output = "Angka yang anda fikirkan adalah 3";}
+
+if ( (condt) == 4)
+{output = "Angka yang anda fikirkan adalah 4";}
+
+if ( (condt) == 5)
+{output = "Angka yang anda fikirkan adalah 5";}
+
+if ( (condt) == 6)
+{output = "Angka yang anda fikirkan adalah 6";}
+
+if ( (condt) == 7)
+{output = "Angka yang anda fikirkan adalah 7";}
+
+if ( (condt) == 8)
+{output = "Angka yang anda fikirkan adalah 8";}
+
+
+
+
+
+if ( (condt) == 9)
+{output = "Angka yang anda fikirkan adalah 9";}
+
+if ( (condt) == 10)
+{output = "Angka yang anda fikirkan adalah 10";}
+
+if ( (condt) == 11)
+{output = "Angka yang anda fikirkan adalah 11";}
+
+if ( (condt) == 12)
+{output = "Angka yang anda fikirkan adalah 12";}
+
+if ( (condt) == 13)
+{output = "Angka yang anda fikirkan adalah 13";}
+
+if ( (condt) == 14)
+{output = "Angka yang anda fikirkan adalah 14";}
+
+if ( (condt) == 15)
+{output = "Angka yang anda fikirkan adalah 15";}
+
+if ( (condt) == 16)
+{output = "Angka yang anda fikirkan adalah 16";}
+
+
+
+
+
+if ( (condt) == 17)
+{output = "Angka yang anda fikirkan adalah 17";}
+
+if ( (condt) == 18)
+{output = "Angka yang anda fikirkan adalah 18";}
+
+if ( (condt) == 19)
+{output = "Angka yang anda fikirkan adalah 19";}
+
+if ( (condt) == 20)
+{output = "Angka yang anda fikirkan adalah 20";}
+
+if ( (condt) == 21)
+{output = "Angka yang anda fikirkan adalah 21";}
+
+if ( (condt) == 22)
+{output = "Angka yang anda fikirkan adalah 22";}
+
+if ( (condt) == 23)
+{output = "Angka yang anda fikirkan adalah 23";}
+
+if ( (condt) == 24)
+{output = "Angka yang anda fikirkan adalah 24";}
+
+
+
+
+
+if ( (condt) == 25)
+{output = "Angka yang anda fikirkan adalah 25";}
+
+if ( (condt) == 26)
+{output = "Angka yang anda fikirkan adalah 26";}
+
+if ( (condt) == 27)
+{output = "Angka yang anda fikirkan adalah 27";}
+
+if ( (condt) == 28)
+{output = "Angka yang anda fikirkan adalah 28";}
+
+if ( (condt) == 29)
+{output = "Angka yang anda fikirkan adalah 29";}
+
+if ( (condt) == 30)
+{output = "Angka yang anda fikirkan adalah 30";}
+
+if ( (condt) == 31)
+{output = "Angka yang anda fikirkan adalah 31";}
+
+if ( (condt) == 32)
+{output = "Angka yang anda fikirkan adalah 32";}
+
+
+
+
+
+if ( (condt) == 33)
+{output = "Angka yang anda fikirkan adalah 33";}
+
+if ( (condt) == 34)
+{output = "Angka yang anda fikirkan adalah 34";}
+
+if ( (condt) == 35)
+{output = "Angka yang anda fikirkan adalah 35";}
+
+if ( (condt) == 36)
+{output = "Angka yang anda fikirkan adalah 36";}
+
+if ( (condt) == 37)
+{output = "Angka yang anda fikirkan adalah 37";}
+
+if ( (condt) == 38)
+{output = "Angka yang anda fikirkan adalah 38";}
+
+if ( (condt) == 39)
+{output = "Angka yang anda fikirkan adalah 39";}
+
+if ( (condt) == 40)
+{output = "Angka yang anda fikirkan adalah 40";}
+
+
+
+
+
+if ( (condt) == 41)
+{output = "Angka yang anda fikirkan adalah 41";}
+
+if ( (condt) == 42)
+{output = "Angka yang anda fikirkan adalah 42";}
+
+if ( (condt) == 43)
+{output = "Angka yang anda fikirkan adalah 43!";}
+
+if ( (condt) == 44)
+{output = "Angka yang anda fikirkan adalah 44";}
+
+if ( (condt) == 45)
+{output = "Angka yang anda fikirkan adalah 45";}
+
+if ( (condt) == 46)
+{output = "Angka yang anda fikirkan adalah 46";}
+
+if ( (condt) == 47)
+{output = "Angka yang anda fikirkan adalah 47";}
+
+if ( (condt) == 48)
+{output = "Angka yang anda fikirkan adalah 48";}
+
+
+
+
+
+if ( (condt) == 49)
+{output = "Angka yang anda fikirkan adalah 49";}
+
+if ( (condt) == 50)
+{output = "Angka yang anda fikirkan adalah 50";}
+
+if ( (condt) == 51)
+{output = "Angka yang anda fikirkan adalah 51";}
+
+if ( (condt) == 52)
+{output = "Angka yang anda fikirkan adalah 52";}
+
+if ( (condt) == 53)
+{output = "Angka yang anda fikirkan adalah 53";}
+
+if ( (condt) == 54)
+{output = "Angka yang anda fikirkan adalah 54";}
+
+if ( (condt) == 55)
+{output = "Angka yang anda fikirkan adalah 55";}
+
+if ( (condt) == 56)
+{output = "Angka yang anda fikirkan adalah 56";}
+
+
+
+
+
+if ( (condt) == 57)
+{output = "Angka yang anda fikirkan adalah 57";}
+
+if ( (condt) == 58)
+{output = "Angka yang anda fikirkan adalah 58";}
+
+if ( (condt) == 59)
+{output = "Angka yang anda fikirkan adalah 59";}
+
+if ( (condt) == 60)
+{output = "Angka yang anda fikirkan adalah 60";}
+
+if ( (condt) == 61)
+{output = "Angka yang anda fikirkan adalah 61";}
+
+if ( (condt) == 62)
+{output = "Angka yang anda fikirkan adalah 62";}
+
+if ( (condt) == 63)
+{output = "Angka yang anda fikirkan adalah 63";}
+
+(document.guicheck.output.value = output);
+
+// // image
+
+// eval("document.img.src=\"" + condt + ".png\"");
+// eval("document.img2.src=\"" + condt + ".png\"");
+// eval("document.img3.src=\"" + condt + ".png\"");
+// eval("document.img4.src=\"" + condt + ".png\"");
+// eval("document.img5.src=\"" + condt + ".png\"");
+// eval("document.img6.src=\"" + condt + ".png\"");
+// eval("document.img7.src=\"" + condt + ".png\"");
+// eval("document.img8.src=\"" + condt + ".png\"");
+
+// eval("document.img9.src=\"" + condt + ".png\"");
+// eval("document.img10.src=\"" + condt + ".png\"");
+// eval("document.img11.src=\"" + condt + ".png\"");
+// eval("document.img12.src=\"" + condt + ".png\"");
+// eval("document.img13.src=\"" + condt + ".png\"");
+// eval("document.img14.src=\"" + condt + ".png\"");
+// eval("document.img15.src=\"" + condt + ".png\"");
+// eval("document.img16.src=\"" + condt + ".png\"");
+
+// eval("document.img17.src=\"" + condt + ".png\"");
+// eval("document.img18.src=\"" + condt + ".png\"");
+// eval("document.img19.src=\"" + condt + ".png\"");
+// eval("document.img20.src=\"" + condt + ".png\"");
+// eval("document.img21.src=\"" + condt + ".png\"");
+// eval("document.img22.src=\"" + condt + ".png\"");
+// eval("document.img23.src=\"" + condt + ".png\"");
+// eval("document.img24.src=\"" + condt + ".png\"");
+
+// eval("document.img25.src=\"" + condt + ".png\"");
+// eval("document.img26.src=\"" + condt + ".png\"");
+// eval("document.img27.src=\"" + condt + ".png\"");
+// eval("document.img28.src=\"" + condt + ".png\"");
+// eval("document.img29.src=\"" + condt + ".png\"");
+// eval("document.img30.src=\"" + condt + ".png\"");
+// eval("document.img31.src=\"" + condt + ".png\"");
+// eval("document.img32.src=\"" + condt + ".png\"");
+		
+}
+
+
+
+//-->
